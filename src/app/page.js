c3df757baa9b235c9c5441a762a6b386937a8590
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-04-17 20:39:15 +0300

    start

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,10 @@
 import Link from "next/link";
 
-import styles from "./page.module.css";
-
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
       <h1>I want learn NEXT</h1>
-        <ul>
-            <li><Link href={'/users'}> Users</Link></li>
-            <li><Link href={'/id'}>ID</Link></li>
-            <li><Link href={'/users/posts'}>USERS-POSTS</Link></li>
-        </ul>
     </main>
   );
 }
--- src/app/users/posts/page.js
+++
@@ -1,9 +0,0 @@
-const Posts = ({params}) => {
-    return (
-        <main>
-            <h1>POSTS</h1>
-        </main>
-    );
-};
-
-export default Posts;
