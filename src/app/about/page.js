fbc74394410ef69b0c5335f709dc68dbf7308797
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-04-17 21:32:28 +0300

    start

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -3,6 +3,7 @@ const About = () => {
     return (
         <main>
             <h1>About page</h1>
+
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti, eum.</p>
         </main>
     );
