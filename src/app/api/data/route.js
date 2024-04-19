fd9e3c6b11181b8edb1d4bc1cad3204646ebfc2f
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-04-19 15:31:17 +0300

    crud

---
+++ src/app/api/data/route.js
@@ -0,0 +1,22 @@
+export async function GET(request) {
+    const data = {
+        name: 'Bob',
+        age: 25,
+        bio: 'some info'
+    }
+
+    return new Response(JSON.stringify(data));
+}
+
+export async function POST(request) {
+    return new Response(request.body);
+}
+
+export async function PUT(request) {
+    return new Response(request.body);
+}
+
+export async function DELETE(request) {
+    return new Response(request.body);
+}
+
