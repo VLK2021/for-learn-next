c9238868526594623941684fa3987647eccb3b8e
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-05-02 19:37:47 +0300

    authentication

--- src/app/configs/auth.js
+++ src/app/configs/auth.js
@@ -1,6 +1,8 @@
 import GoogleProvider from "next-auth/providers/google";
 import Credentials from "next-auth/providers/credentials";
 
+import {users} from "../data/users";
+
 
 
 export const authConfig = {
@@ -13,7 +15,24 @@ export const authConfig = {
             credentials: {
                 email: {label: 'email', type: 'email', required: true},
                 password: {label: 'password', type: 'password', required: true},
+            },
+            async authorize(credentials) {
+               if (!credentials?.email || !credentials.password) return null;
+
+               const currentUser = users.find(user => user.email === credentials.email);
+
+                if (currentUser && currentUser.password === credentials.password) {
+                    const { password, ...userWithoutPass } = currentUser;
+
+                    return userWithoutPass;
+                }
+
+
+                return null
             }
         }),
-    ]
+    ],
+    pages: {
+        signIn: '/signin'
+    }
 }
---
+++ src/app/signin/page.js
@@ -0,0 +1,14 @@
+import {GoogleButton} from "../components/GoogleButton";
+import {SignInForm} from "../components/SignInForm";
+
+
+export default async function Signin() {
+    return (
+        <div className="stack">
+            <h1>Sign In</h1>
+            <GoogleButton />
+            <div>or</div>
+            <SignInForm />
+        </div>
+    )
+}
